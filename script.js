378c26be44bb8e13e8d95ec88f7750a3dd770aa7
Volha Sakharevich <vuictorovna@gmail.com>
2023-09-10 22:49:50 +0100

    Add email form validaton

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const postcodeInput = document.getElementById('postcode');
+    const constituencyInput = document.getElementById('constituency');
     const emailButton = document.getElementById('emailButton');
     const firstNameInput = document.getElementById('firstName');
     const lastNameInput = document.getElementById('lastName');
+
+    function isValidForm() {
+        let valid = true;
+
+        const firstName = firstNameInput.value.trim();
+        const lastName = lastNameInput.value.trim();
+        const constituency = constituencyInput.value.trim();
+
+        // Validate First Name
+        if (!firstName) {
+            document.getElementById('firstNameError').textContent = 'First name must be provided';
+            valid = false;
+        } else if (/\d/.test(firstName)) {
+            document.getElementById('firstNameError').textContent = 'First name cannot contain numbers';
+            valid = false;
+        } else {
+            document.getElementById('firstNameError').textContent = '';
+        }
+
+        // Validate Last Name
+        if (!lastName) {
+            document.getElementById('lastNameError').textContent = 'Last name must be provided';
+            valid = false;
+        } else if (/\d/.test(lastName)) {
+            document.getElementById('lastNameError').textContent = 'Last name cannot contain numbers';
+            valid = false;
+        } else {
+            document.getElementById('lastNameError').textContent = '';
+        }
+
+        // Validate constituency
+        if (!constituency) {
+            document.getElementById('constituencyError').textContent = 'constituency must be provided';
+            valid = false;
+        } else {
+            document.getElementById('constituencyError').textContent = '';
+        }
+
+        return valid;
+    }
+
 
     function fetchContactDetails(memberId, mpName) {
         fetch(`https://members-api.parliament.uk/api/Members/${memberId}/Contact`)
@@ -19,18 +60,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     emailButton.addEventListener('click', () => {
-        const query = encodeURIComponent(postcodeInput.value.trim());
+        if (isValidForm()) { 
+            const query = encodeURIComponent(constituencyInput.value.trim());
 
-        fetch(`https://members-api.parliament.uk/api/Location/Constituency/Search?searchText=${query}&skip=0&take=1`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.items && data.items.length > 0) {
-                const mpInfo = data.items[0].value.currentRepresentation.member.value;
-                const mpName = mpInfo.nameDisplayAs;
-                const memberId = mpInfo.id;
+            fetch(`https://members-api.parliament.uk/api/Location/Constituency/Search?searchText=${query}&skip=0&take=1`)
+        
+            .then(response => response.json())
+            .then(data => {
+                if (data.items && data.items.length > 0) {
+                    const mpInfo = data.items[0].value.currentRepresentation.member.value;
+                    const mpName = mpInfo.nameDisplayAs;
+                    const memberId = mpInfo.id;
 
-                fetchContactDetails(memberId, mpName);
-            }
-        });
+                    fetchContactDetails(memberId, mpName);
+                }
+            });
+        }
     });
 });
